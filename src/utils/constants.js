6c2d1bcd03ce6f2789abd0175d0cd946f9530c6a
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2025-05-08 12:28:25 +0530

    changed routes path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const PRODUCT_API = {
   LOGIN: `${API_BASE_URL}/login`,
